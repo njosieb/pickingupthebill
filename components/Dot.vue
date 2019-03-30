f273e988e3d729142cee528e2f122de33712d385
Nina Harris <ninaharris121@gmail.com>
2019-03-29 20:04:36 -0500

    Makes the dots a component

---
+++ components/Dot.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    :style="{height: `${$props.size}px`, width: `${$props.size}px`, top: `${$props.top}%`, left: `${$props.left}%`}"
+    class="circle black"
+  >{{ $props.dotId }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    dotId: Number,
+    size: Number,
+    top: Number,
+    left: Number
+  }
+}
+</script>
+
+<style>
+.circle {
+  position: absolute;
+  border-radius: 50%;
+  z-index: 1;
+  background-color: #d6158a;
+}
+</style>
