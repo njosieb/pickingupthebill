d2cbf11ea2d7f04f29d98b370146bae80f545749
Nina Harris <ninaharris121@gmail.com>
2019-04-12 20:47:19 -0500

    Adds Footer

---
+++ components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    <slot></slot>
+  </footer>
+</template>
+
+<script>
+import Container from '../components/Container'
+export default {
+  components: {
+    Container
+  }
+  // data: function() {
+  //   return {
+
+  //   }
+  //   }
+}
+</script>
+
+<style lang='scss'>
+</style>
